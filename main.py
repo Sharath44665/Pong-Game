cad632bdb4e2541b961f44a13bcb277fe8abf511
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-12 12:20:46 +0530

    fix few bugs in the code

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ my_screen.bgcolor("black")
 my_screen.title(titlestring="Pong Game")
 my_screen.tracer(0) #tracer is off meaning animation off
 
-right_slider=Slider()
+right_slider=Slider(x_val=350,y_val=0)
+left_slider=Slider(x_val=-350,y_val=0)
 my_screen.listen()
 
 my_screen.onkey(fun=right_slider.move_up, key="Up")
 my_screen.onkey(fun=right_slider.move_down, key="Down")
+my_screen.onkey(fun=left_slider.move_up, key="w")
+my_screen.onkey(fun=left_slider.move_down, key="s")
 
 game_status=True
 while game_status:
--- slider.py
+++ slider.py
@@ -6,28 +6,27 @@ DISTANCE = 10
 
 
 class Slider(Turtle):
-    def __init__(self):
+    def __init__(self, x_val, y_val):
         super().__init__()
-        self.slider = self.create_right_slider()
-        self.new_y=0
+        self.slider_pos = (x_val, y_val)
+        self.create_slider(self.slider_pos)
+        self.new_y = 0
 
-    def create_right_slider(self):
-        slider = Turtle()
-        slider.shape(name="square")
-        slider.width = 20
-        slider.penup()
-        slider.color("white")
-        slider.shapesize(stretch_wid=5, stretch_len=1)
-        slider.goto(x=350, y=0)
-        return slider
 
-    # def move(self):
-    #     self.slider.forward(DISTANCE)
+    def create_slider(self, slider_pos):
+        self.shape(name="square")
+        self.width = 20
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        posx = slider_pos[0]
+        posy = slider_pos[1]
+        self.goto(x=posx, y=posy)
 
     def move_up(self):
-        self.new_y+=20
-        self.slider.goto(x=350,y=self.new_y)
+        self.new_y += 20
+        self.goto(x=self.slider_pos[0], y=self.new_y)
 
     def move_down(self):
         self.new_y -= 20
-        self.slider.goto(x=350, y=self.new_y)
+        self.goto(x=self.slider_pos[0], y=self.new_y)
