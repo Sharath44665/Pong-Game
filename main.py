b2220463d36df7d6e59fa6cd9f924e512f1c5cdb
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-12 11:17:17 +0530

    create right slider and move up or down

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ my_screen.setup(width=800,height=600)
 my_screen.bgcolor("black")
 my_screen.title(titlestring="Pong Game")
 
-right_slider=Slider(val_left_or_right="right")
-left_slider=Slider(val_left_or_right="left")
+right_slider=Slider()
 my_screen.listen()
-my_screen.onkey(fun=right_slider.move_up, key="Up")
-my_screen.onkey(fun=right_slider.move_down,key="Down")
-# my_screen.onkey(fun=left_slider.r_slider, key="r")
 
-print(my_screen.onkey(fun=right_slider.move_up, key="Up"))
+my_screen.onkey(fun=right_slider.move_up, key="Up")
+my_screen.onkey(fun=right_slider.move_down, key="Down")
 
 game_status=True
 while game_status:
--- slider.py
+++ slider.py
@@ -1,52 +1,31 @@
 from turtle import Turtle
 
-DISTANCE = 10
 UP = 90
 DOWN = 270
-LEFT = 180
-RIGHT = 0
+DISTANCE = 10
 
 
 class Slider(Turtle):
-    def __init__(self,val_left_or_right):
+    def __init__(self):
         super().__init__()
-        self.r_slider = []
+        self.slider = self.create_right_slider()
 
-        if val_left_or_right=="right":
-            self.create_slider(380)
-        elif val_left_or_right=="left":
-            self.create_slider(-380)
-
-        self.r_slider_head = self.r_slider[0]
-
-    def create_slider(self,x_val):
-        for _ in range(3):
-            slider = Turtle()
-            slider.shape(name="square")
-            slider.penup()
-            slider.color("white")
-            self.r_slider.append(slider)
-
-        y_val = 0
-
-        for l in self.r_slider:
-            l.goto(x=x_val, y=y_val)
-            y_val += 20
+    def create_right_slider(self):
+        slider = Turtle()
+        slider.shape(name="square")
+        slider.width = 20
+        slider.penup()
+        slider.color("white")
+        slider.goto(x=350, y=0)
+        return slider
 
     def move(self):
-        for index_of_slider in range(len(self.r_slider) - 1, 0, -1):
-            x_val = self.r_slider[index_of_slider - 1].xcor()
-            y_val = self.r_slider[index_of_slider - 1].ycor()
-            self.r_slider[index_of_slider].goto(x=x_val, y=y_val)
-        self.r_slider_head.forward(DISTANCE)
+        self.slider.forward(DISTANCE)
 
     def move_up(self):
-        self.r_slider_head.setheading(UP)
+        self.slider.setheading(UP)
         self.move()
 
     def move_down(self):
-        self.r_slider_head.setheading(DOWN)
+        self.slider.setheading(DOWN)
         self.move()
-
-    def right_slider(self):
-        pass
